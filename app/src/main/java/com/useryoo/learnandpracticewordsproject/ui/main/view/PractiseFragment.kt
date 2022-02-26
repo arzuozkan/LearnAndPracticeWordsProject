43331da41c4b2188b85b2114c1193b821ceb8668
Arzu O <arzuozkan1905@gmail.com>
2022-02-26 18:36:09 +0300

    change learnFragment icon and implement bottom_nav with fragments

---
+++ app/src/main/java/com/useryoo/learnandpracticewordsproject/ui/main/view/PractiseFragment.kt
@@ -0,0 +1,19 @@
+package com.useryoo.learnandpracticewordsproject.ui.main.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.useryoo.learnandpracticewordsproject.R
+class PractiseFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_practise, container, false)
+    }
+
+
+}
